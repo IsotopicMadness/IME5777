ce6763f9a9f7553893fca6f69c511b48f01ff577
Amichai <amichaibarhaim@gmail.com>
2018-05-04 17:36:43 +0300

    Added JavaDoc to Camera

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -2,7 +2,9 @@ package elements;
 import primitives.*;
 
 /**
- * 
+ * Camera.
+ * Used as a substitute for a real eye.
+ * Used mainly for the math used to create the image through the grid
  *
  */
 
@@ -13,7 +15,10 @@ public class Camera {
 	Vector vTo;
 	Vector vRight;	
 	
-	
+	/**
+	 * Constructor.
+	 * Receives the camera's location (p0), Upwards vector (vUp), The vector that points towards the grid/screen (vTo) and calculates vRight, the vector Vertical to both vTo and vUp.
+	 */
 	public Camera(Point3D p0, Vector vUp, Vector vTo/*, Vector vRight*/) {
 		this.p0 = new Point3D(p0);
 		this.vUp = new Vector(vUp).normalize();
@@ -25,6 +30,17 @@ public class Camera {
 			vRight = vUp.crossProduct(vTo).normalize();
 	}
 	
+	/**
+	 * The function constructs a ray through a given pixel in the given grid
+	 * @param Nx
+	 * @param Ny
+	 * @param i
+	 * @param j
+	 * @param screenDistance
+	 * @param screenWidth
+	 * @param screenHeight
+	 * @return
+	 */
 	public Ray constructRayThroughPixel(int Nx, int Ny, int i, int j, double screenDistance, double screenWidth, double screenHeight) {
 		
 		Point3D pC = new Point3D(
