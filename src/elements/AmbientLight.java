1de4a61c61be7cc6f8af82829e6d55896d34e0cf
איתי <@DESKTOP-PUS8OUE>
2018-05-08 19:10:37 +0300

    nitzan

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -1,5 +1,38 @@
 package elements;
 
-public class AmbientLight {
+import primitives.Color;
 
+public class AmbientLight {
+	private Color _color;
+	private double _Ka;
+	private Color _intensity;
+	
+	/********** Constructors ***********/	
+	public AmbientLight(Color color, double ka) {
+		_color = color;
+		_Ka = ka;
+		_intensity = new Color(_color).scale(_Ka);
+	}
+
+	/************** Getters/Setters *******/
+	public Color get_color() {
+		return _color;
+	}
+
+	public void set_color(Color _color) {
+		this._color = _color;
+	}
+
+	public double getKa() {
+		return _Ka;
+	}
+
+	public void setKa(double ka) {
+		_Ka = ka;
+	}
+	// retuurn final Ambiant light after multiply in Ka 
+	public Color getIntensity()
+	{
+		return _intensity;
+	}
 }
