7a188299aa0cae42779ad15805825dea3017333d
amich <amich@AmihaisOmen>
2018-06-04 19:52:55 +0300

    Added translucency and opacity variables

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -4,17 +4,23 @@ public class Material {
 	protected double _Kd;
 	protected double _Ks;
 	protected int _nShininess;
+	protected double _Kr;
+	protected double _Kt;
 	
-	public Material(double kd, double ks, int nShininess) {
+	public Material(double kd, double ks, int nShininess, double kr, double kt) {
 		_Kd = kd;
 		_Ks = ks;
 		_nShininess = nShininess;
+		_Kr = kr;
+		_Kt = kt;
 	}
 	
 	public Material(Material other) {
 		_Kd = other.getKd();
 		_Ks = other.getKs();
 		_nShininess = other.getShininess();
+		_Kr = other.get_Kr();
+		_Kt = other.get_Kt();
 	}
 
 	public int getShininess() {
@@ -30,5 +36,11 @@ public class Material {
 	}
 	public Material getMaterial() {
 		return this;
+	}
+	public double get_Kr() {
+		return _Kr;
+	}
+	public double get_Kt() {
+		return _Kt;
 	}
 }
