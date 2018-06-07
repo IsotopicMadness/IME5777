f5c9582a63462144b41a11fd8a8a74d5a5d27b3a
amich <amich@AmihaisOmen>
2018-06-07 18:06:45 +0300

    Added JavaDoc

--- src/primitives/Point2D.java
+++ src/primitives/Point2D.java
@@ -1,11 +1,5 @@
 package primitives;
 
-/**
- * skj hfgkhl jdflkh jdfhj fd kh
- * 
- * @author amich
- *
- */
 public class Point2D {
 	protected Coordinate x;
 	protected Coordinate y;
@@ -19,23 +13,39 @@ public class Point2D {
 		this.x = new Coordinate(x);
 		this.y = new Coordinate(y);
 	}
-	
+
 	public Point2D(Point2D other) {
 		x = new Coordinate(other.getX());
 		y = new Coordinate(other.getY());
 	}
 
 	// get/set
+
+	/**
+	 * returns the X value of the point
+	 * 
+	 * @return
+	 */
 	public Coordinate getX() {
 		return x;
 	}
 
+	/**
+	 * returns the Y value of the point
+	 * 
+	 * @return
+	 */
 	public Coordinate getY() {
 		return y;
 	}
 
 	// Overrides
 	@Override
+	/**
+	 * Checks if the given point shares the same values as this one
+	 * 
+	 * @return
+	 */
 	public boolean equals(Object obj) {
 		if (obj == null)
 			return false;
@@ -44,22 +54,36 @@ public class Point2D {
 		if (!(obj instanceof Point2D))
 			return false;
 		Point2D other = (Point2D) obj;
-		return other.x.equals(this.x) && other.y.equals(this.y);
+		return other.x.equals(x) && other.y.equals(y);
 	}
 
 	@Override
+	/**
+	 * returns the point's string
+	 */
 	public String toString() {
 		return "(" + x.toString() + ", " + y.toString() + ")";
 	}
 
 	// operations
+	/**
+	 * subtracts the values of the given point from @this
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public Point2D substract(Point2D other) {
-		return new Point2D(new Coordinate(this.x.subtract(other.getX().getNum())),
-				new Coordinate(this.y.subtract(other.getY().getNum())));
+		return new Point2D(new Coordinate(x.subtract(other.getX().getNum())),
+				new Coordinate(y.subtract(other.getY().getNum())));
 	}
 
+	/**
+	 * Adds the values of the given point to @this
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public Point2D add(Point2D other) {
-		return new Point2D(new Coordinate(this.x.add(other.getX().getNum())),
-				new Coordinate(this.y.add(other.getY().getNum())));
+		return new Point2D(new Coordinate(x.add(other.getX().getNum())), new Coordinate(y.add(other.getY().getNum())));
 	}
 }
