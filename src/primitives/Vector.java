e7d813432189d4e2c5e98f562c6ec3e62ffbffcb
amich <amich@AmihaisOmen>
2018-06-07 19:04:09 +0300

    added Javadoc

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -33,52 +33,99 @@ public class Vector extends Point3D {
 	}
 
 	// getters/setters
+	/**
+	 * returns the length of the vector
+	 * 
+	 * @return
+	 */
 	public double getLength() {
 		return length;
 	}
 
 	// admin
+	/**
+	 * Calculates the length of the vector
+	 * 
+	 * @return
+	 */
 	private double _length() {
 		return super.distance(Point3D.ZERO);
 	}
 
 	// overrides
 	@Override
+	/**
+	 * checks whether the given object has the same values as this
+	 */
 	public boolean equals(Object obj) {
 		if (obj == null)
 			return false;
-		if (this == obj)
-			return true;
 		if (!(obj instanceof Vector))
 			return false;
+		if (this == obj)
+			return true;
 		return super.equals((Vector) obj);
 	}
 
-	// operations
+	/**
+	 * Adds a given vector to this
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public Vector add(Vector other) {
 		return new Vector(x.add(other.x), y.add(other.y), z.add(other.z));
 	}
 
+	/**
+	 * Subtracts a given vector from this
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public Vector subtract(Vector other) {
-		return new Vector(new Coordinate(getX().add(-other.getX().getNum())),
-				new Coordinate(getY().add(-other.getY().getNum())), new Coordinate(getZ().add(-other.getZ().getNum())));
+		return new Vector(new Coordinate(x.add(-other.x.getNum())), new Coordinate(y.add(-other.y.getNum())),
+				new Coordinate(z.add(-other.z.getNum())));
 	}
 
+	/**
+	 * Returns a vector
+	 * 
+	 * @param lambda
+	 * @return
+	 */
 	public Vector scale(double lambda) {
 		return new Vector(x.mult(lambda), y.mult(lambda), z.mult(lambda));
 	}
 
+	/**
+	 * returns the dot product of a given vector and this one
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public double dotProduct(Vector other) {
 		return x.mult(other.x) + y.mult(other.y) + z.mult(other.z);
 	}
 
 	// Double check result
+	/**
+	 * Returns the new cross product of the given vector and this one
+	 * 
+	 * @param other
+	 * @return
+	 */
 	public Vector crossProduct(Vector other) {
-		return new Vector(new Coordinate(getY().mult(other.getZ().getNum()) - getZ().mult(other.getY().getNum())),
-				new Coordinate(getX().mult(other.getZ().getNum()) - getZ().mult(other.getX().getNum())),
-				new Coordinate(getX().mult(other.getY().getNum()) - getY().mult(other.getX().getNum())));
+		return new Vector(new Coordinate(y.mult(other.z.getNum()) - z.mult(other.y.getNum())),
+				new Coordinate(x.mult(other.z.getNum()) - z.mult(other.x.getNum())),
+				new Coordinate(x.mult(other.y.getNum()) - y.mult(other.x.getNum())));
 	}
 
+	/**
+	 * Normalises the vector
+	 * 
+	 * @return
+	 */
 	public Vector normalize() {
 		return this.scale(1 / length);
 	}
