3dc2ffec1418697674597c2787bf510ef5f7657d
amich <amich@AmihaisOmen>
2018-06-07 19:27:47 +0300

    Added JavaDoc

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -15,7 +15,7 @@ public class Material {
 		_Kr = kr > 1 ? 1 : (kt < 0 ? -1 : kt);
 		_Kt = kt > 1 ? 1 : (kt < 0 ? -1 : kt);
 		if (_Kr == -1 || _Kt == -1)
-			throw new ArgumentException("Kt and Kr values are incorrect");
+			throw new IllegalArgumentException("Kt and Kr values are incorrect");
 	}
 
 	public Material(Material other) {
@@ -26,26 +26,42 @@ public class Material {
 		_Kt = other.get_Kt();
 	}
 
+	/**
+	 * returns the shininess of the Material
+	 * @return
+	 */
 	public int getShininess() {
 		return _nShininess;
 	}
 
+	/**
+	 * Returns the material's Ks
+	 * @return
+	 */
 	public double getKs() {
 		return _Ks;
 	}
 
+	/**
+	 * returns the material's Kd
+	 * @return
+	 */
 	public double getKd() {
 		return _Kd;
 	}
 
-	public Material getMaterial() {
-		return this;
-	}
-
+	/**
+	 * returns the material's Kr
+	 * @return
+	 */
 	public double get_Kr() {
 		return _Kr;
 	}
 
+	/**
+	 * Returns the Material's Kt
+	 * @return
+	 */
 	public double get_Kt() {
 		return _Kt;
 	}
