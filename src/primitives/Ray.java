77c6f2ff8eeec6c928a0a67cd84bee085c3701b2
amich <amich@AmihaisOmen>
2018-06-07 19:27:59 +0300

    Fixes

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -4,39 +4,63 @@ public class Ray extends Vector {
 
 	private Point3D location;
 
-	//Constructors
+	// Constructors
 	public Ray(Vector direction, Point3D point) {
 		super(direction.normalize());
-		location=new Point3D(point);
+		location = new Point3D(point);
 	}
 
 	public Ray(Ray other) {
 		super(other);
-		location=new Point3D(other.getLocation());
+		location = new Point3D(other.getLocation());
 	}
 
-	//get/set
+	// get/set
+
+	/**
+	 * returns the point of origins of the ray
+	 * 
+	 * @return
+	 */
 	public Point3D getLocation() {
 		return location;
 	}
+
+	/**
+	 * returns the vector
+	 * 
+	 * @return
+	 */
 	public Vector getDirection() {
-		return new Vector(getX(),getY(),getZ());
+		return new Vector(x, y, z);
 	}
 
-	//Overrides
+	// Overrides
 	@Override
+	/**
+	 * checks if the given object is a ray and if it's values are equal to this
+	 * 
+	 * @param obj
+	 * @return
+	 */
 	public boolean equals(Object obj) {
-		if(obj==null)
+		if (obj == null)
 			return false;
-		if(!(obj instanceof Ray))
+		if (!(obj instanceof Ray))
 			return false;
-		if(this==obj)
+		if (this == obj)
 			return true;
-		Ray other = new Ray((Ray)obj);
-		return this.location.equals(other.getLocation()) && super.equals(getDirection());	
+		Ray other = new Ray((Ray) obj);
+		return this.location.equals(other.getLocation()) && super.equals(getDirection());
 	}
+
 	@Override
+	/**
+	 * returns the string of this object
+	 * 
+	 * @return
+	 */
 	public String toString() {
-		return "Direction: "+super.toString()+"\nLocation: "+location.toString();
+		return "Direction: " + super.toString() + "\nLocation: " + location.toString();
 	}
 }
