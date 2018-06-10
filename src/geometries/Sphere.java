f5bbac2bc6370f174e5f9d41597cd856fde5d607
amich <amich@AmihaisOmen>
2018-06-10 14:42:02 +0300

    Bug fixes and refactory in order to make cylinder

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,8 +1,9 @@
 package geometries;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import com.sun.javafx.collections.MappingChange.Map;
+import java.util.List;
+import java.util.Map;
 
 import primitives.*;
 
@@ -10,52 +11,59 @@ import primitives.*;
  * 3D object: Sphere
  *
  */
-public class Sphere extends RadialGeometry{
-	//Constructors
+public class Sphere extends RadialGeometry {
+
+	private Point3D _center;
+
+	// Constructors
 	public Sphere(Point3D center, double radius, Color color, Material material) {
-		super(radius, new Point3D(center), color, material);
+		super(radius, color, material);
+		_center = new Point3D(center);
 	}
+
 	public Sphere(Sphere other) {
-		super(other.getRadius(),new Point3D(other.getPoint()), other.getEmmission(), other.getMaterial());
+		super(other.getRadius(), other.getEmmission(), other);
+		_center = new Point3D(other._center);
+
 	}
-	
-	//Overrides
-	
+
+	// Overrides
+
 	@Override
-	/**returns normal from a given point
+	/**
+	 * returns normal from a given point
+	 * 
 	 * @param Point3D
 	 */
 	public Vector getNormal(Point3D p) {
-		//if(p.distance(this.getPoint())!= this.getRadius())
-			//throw new IllegalArgumentException("Point is not on sphere");
-		//else
-			return (new Vector(p.subtract(this.getPoint())).normalize());
+		// if(p.distance(this.getPoint())!= this.getRadius())
+		// throw new IllegalArgumentException("Point is not on sphere");
+		// else
+		return (new Vector(p.subtract(_center)).normalize());
 	}
+
 	@Override
 	/**
 	 * @param ray
-	 * Calculates and returns the points where the given ray crosses the Sphere
+	 *            Calculates and returns the points where the given ray crosses the
+	 *            Sphere
 	 */
-	public Map<Geometry,ArrayList<Point3D>> findIntersection(Ray ray) {
-		
-		HashMap<Geometry,ArrayList<Point3D>> result = new HashMap<Geometry,ArrayList<Point3D>>();
-		
-		Vector u = new Vector(this.getPoint().subtract(ray.getLocation()));
-		ray=new Ray(ray.normalize(),ray.getLocation());
-		double tm = (u.dotProduct(ray.getDirection()));
-		double d = Math.sqrt(
-				Math.pow(u.getLength(), 2)-
-				Math.pow(tm, 2));
+	public Map<Intersectable, List<Point3D>> findIntersection(Ray ray) {
 
-		if( d > this.getRadius())
+		Map<Intersectable, List<Point3D>> result = new HashMap<Intersectable, List<Point3D>>();
+
+		Vector u = new Vector(_center.subtract(ray.getLocation()));
+		ray = new Ray(ray.normalize(), ray.getLocation());
+		double tm = (u.dotProduct(ray.getDirection()));
+		double d = Math.sqrt(Math.pow(u.getLength(), 2) - Math.pow(tm, 2));
+
+		if (d > this.getRadius())
 			return result;
-		double th = Math.sqrt(
-				Math.pow(this.getRadius(),2)
-				-Math.pow(d, 2));
-		double t1 = tm+th;
-		double t2 = tm-th;
-		
-		if(t1!=0){
+		double th = Math.sqrt(Math.pow(this.getRadius(), 2) - Math.pow(d, 2));
+		double t1 = tm + th;
+		double t2 = tm - th;
+
+		if (t1 != 0) {
 			ArrayList<Point3D> array = new ArrayList<>();
 			array.add(new Point3D(ray.getLocation().add(ray.getDirection().scale(t1))));
 			array.add(new Point3D(ray.getLocation().add(ray.getDirection().scale(t2))));
@@ -63,7 +71,7 @@ public class Sphere extends RadialGeometry{
 		}
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return super.toString();
