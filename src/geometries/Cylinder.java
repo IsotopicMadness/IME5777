91851d89f5def406b3d4cc8941e4b4d024aff7cb
amich <amich@AmihaisOmen>
2018-06-10 14:07:26 +0300

    The Cylinder Reworks

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,60 +1,81 @@
 package geometries;
 
+import java.util.List;
+import java.util.Map;
+
 import primitives.*;
 
 public class Cylinder extends RadialGeometry {
 
-	/**
-	 * Cylinder inherits from RadialGeometry. Therefore the only additional 
-	 */
-	private Vector directionLength;
-	
-	public Cylinder(double r, Point3D p, Vector directionAndLength, Color color, Material material) {
-		super(r, p, color, material);
-		this.directionLength=new Vector(directionAndLength);
+	private double _hight;
+	private Vector _direction;
+	private Point3D _centerPoint1;
+	private Point3D _centerPoint2;
+	private Plane _plane1;
+	private Plane _plane2;
+
+	public Cylinder(double hight, double r, Point3D centerPoint1, Point3D centerPoint2, Plane plane1, Plane plane2,
+			Vector directionAndLength, Color color, Material material) {
+		super(r, color, material);
+		_hight = hight;
+		_direction = new Vector(directionAndLength);
+		_centerPoint1 = new Point3D(centerPoint1);
+		_centerPoint2 = new Point3D(centerPoint2);
+		_plane1 = new Plane(plane1);
+		_plane2 = new Plane(plane2);
+
 	}
 
 	public Cylinder(Cylinder other) {
-		super(other.getRadius(),other.getPoint(), other.getEmmission(), other.getMaterial());
-		this.directionLength = new Vector(other.getDirectionLength());
+		super(other._radius, other.getEmmission(), other);
+		
 	}
 
-	//Getters/setters
+	// Getters/setters
 	public Vector getDirectionLength() {
-		return directionLength;
+		return _direction;
 	}
-	
-	//Overriders
-	
+
+	// Overriders
+
 	/**
 	 * Returns normal
 	 */
 	@Override
 	public Vector getNormal(Point3D p) {
-		Vector v =new Vector(p.subtract(getPoint()));
-		double t=directionLength.dotProduct(v);
-		Point3D q= p.add(directionLength.scale(t));
-		Vector normal= new Vector(p.subtract(q));
+		Vector v = new Vector(p.subtract(getPoint()));
+		double t = _direction.dotProduct(v);
+		Point3D q = p.add(_direction.scale(t));
+		Vector normal = new Vector(p.subtract(q));
 		return normal.normalize();
 	}
-	
+
 	/**
 	 * Checks if equals
 	 */
 	@Override
 	public boolean equals(Object obj) {
-		if(obj==null)
+		if (obj == null)
 			return false;
-		if(!(obj instanceof Cylinder))
+		if (!(obj instanceof Cylinder))
 			return false;
-		if(this==obj)
+		if (this == obj)
 			return true;
-		Cylinder other = new Cylinder((Cylinder)obj);
-		return this.directionLength.equals(other.getDirectionLength()) && this.getPoint() == other.getPoint() && this.getRadius() == other.getRadius();
+		Cylinder other = new Cylinder((Cylinder) obj);
+		return this._direction.equals(other.getDirectionLength()) && this.getPoint() == other.getPoint()
+				&& this.getRadius() == other.getRadius();
 	}
+
 	@Override
 	public String toString() {
-		return "Direction and length: "+directionLength.toString()+", "+directionLength.getLength()+"\nRadius: "+this.getRadius()+"\nPoint: "+getPoint().toString()+"\nColor: "+getEmmission().toString();
+		return "Direction and length: " + _direction.toString() + ", " + _direction.getLength() + "\nRadius: "
+				+ this.getRadius() + "\nPoint: " + getPoint().toString() + "\nColor: " + getEmmission().toString();
+	}
+
+	@Override
+	public Map<Intersectable, List<Point3D>> findIntersection(Ray ray) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -1,48 +1,45 @@
 package geometries;
 
 import primitives.*;
+
 /**
  * The Class acts as an interface for radius-based objects
  *
  */
 public abstract class RadialGeometry extends Geometry {
-	
-	private double _radius;
-	private Point3D point;
-//***************** Constructors ********************** // 
 
-	public RadialGeometry(double r,Point3D p, Color color, Material material) {
+	protected double _radius;
+	// ***************** Constructors ********************** //
+
+	public RadialGeometry(double r, Color color, Material material) {
 		super(color, material);
-		if (r<=0)
+		if (r <= 0)
 			throw new IllegalArgumentException("radius cannot be negative\n");
-		else
-			{
+		else {
 			_radius = r;
-		
-			point = p;
-			}
-	
+
+		}
 	}
+
 	public RadialGeometry(RadialGeometry ra, Material material) {
 		super(ra.getEmmission(), material);
-		this.point = new Point3D(ra.point);
 		this._radius = ra._radius;
-}
-//********* Get/Set*******************//
-	public double getRadius() {return _radius;}
-	
-	public Point3D getPoint() {
-		return point;
 	}
+
+	// ********* Get/Set*******************//
+	public double getRadius() {
+		return _radius;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Radius: "+_radius+", Point: "+point.toString()+"Color: "+getEmmission().toString();
-		}
-	
-	//public void setPoint(Point3D point) {
-		//this.point = point;
-	//}
+		return "Radius: " + _radius + ", " + "Color: " + getEmmission().toString();
+	}
+
+	// public void setPoint(Point3D point) {
+	// this.point = point;
+	// }
 
 	// ***************** Operations ******************** //
 
