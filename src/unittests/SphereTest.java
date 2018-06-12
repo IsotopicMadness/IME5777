e475a98fbb2a6864c52af1a38828168f6e779b50
amich <amich@AmihaisOmen>
2018-06-12 11:23:54 +0300

    Intersectable/Geometry bug fixes

--- src/unittests/SphereTest.java
+++ src/unittests/SphereTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -15,80 +16,71 @@ import elements.*;
 
 public class SphereTest {
 
-	/*@Test
-	public void testGetNormal() {
-		fail("Not yet implemented");
-	}*/
+	/*
+	 * @Test public void testGetNormal() { fail("Not yet implemented"); }
+	 */
 
 	@Test
-	public void testIntersectionPoints(){
+	public void testIntersectionPoints() {
 		final int WIDTH = 3;
 		final int HEIGHT = 3;
-		Ray[][] rays = new Ray [HEIGHT][WIDTH];
-		Camera camera = new Camera(new Point3D(0.0 ,0.0 ,0.0),
-				
-				new Vector (0.0, 1.0, 0.0),
-				new Vector (0.0, 0.0, -1.0));
-		
-		Sphere sphere = new Sphere(new Point3D(0.0, 0.0, -3.0), 1, new Color(), new Material(1,1,1,1,1));
-		Sphere sphere2 = new Sphere(new Point3D(0.0, 0.0, -3.0), 10, new Color(), new Material(1,1,1,1,1));
-	
-		//Only the center ray intersect the sphere in two locations
-		HashMap<Geometry,ArrayList<Point3D>> intersectionPointsSphere = new HashMap<Geometry,ArrayList<Point3D>>();
-		
-		//The sphere encapsulates the view plane - all rays intersect with the sphere once
-		HashMap<Geometry,ArrayList<Point3D>> intersectionPointsSphere2 = new HashMap<Geometry,ArrayList<Point3D>>();
-		
+		Ray[][] rays = new Ray[HEIGHT][WIDTH];
+		Camera camera = new Camera(new Point3D(0.0, 0.0, 0.0),
+
+				new Vector(0.0, 1.0, 0.0), new Vector(0.0, 0.0, -1.0));
+
+		Sphere sphere = new Sphere(new Point3D(0.0, 0.0, -3.0), 1, new Color(), new Material(1, 1, 1, 1, 1));
+		Sphere sphere2 = new Sphere(new Point3D(0.0, 0.0, -3.0), 10, new Color(), new Material(1, 1, 1, 1, 1));
+
+		// Only the center ray intersect the sphere in two locations
+		HashMap<Intersectable, List<Point3D>> intersectionPointsSphere = new HashMap<Geometry, ArrayList<Point3D>>();
+
+		// The sphere encapsulates the view plane - all rays intersect with the sphere
+		// once
+		HashMap<Intersectable, List<Point3D>> intersectionPointsSphere2 = new HashMap<Geometry, ArrayList<Point3D>>();
+
 		System.out.println("Camera:\n" + camera);
-		
-		for (int i = 0; i < HEIGHT; i++){
-			for (int j = 0; j < WIDTH; j++)
-			{
-				
+
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+
 				rays[i][j] = camera.constructRayThroughPixel(
-						
+
 						WIDTH, HEIGHT, j, i, 1, 3 * WIDTH, 3 * HEIGHT);
-				
-				HashMap<Geometry,ArrayList<Point3D>> rayIntersectionPoints = new HashMap<>();
+
+				HashMap<Intersectable, List<Point3D>> rayIntersectionPoints = new HashMap<>();
 				rayIntersectionPoints.putAll(sphere.findIntersection(rays[i][j]));
-				HashMap<Geometry,ArrayList<Point3D>> rayIntersectionPoints2 = new HashMap<>();
+				HashMap<Intersectable, List<Point3D>> rayIntersectionPoints2 = new HashMap<>();
 				rayIntersectionPoints2.putAll(sphere2.findIntersection(rays[i][j]));
-				
-					intersectionPointsSphere.putAll(rayIntersectionPoints);
-				
-					intersectionPointsSphere2.putAll(rayIntersectionPoints2);
-				
+
+				intersectionPointsSphere.putAll(rayIntersectionPoints);
+
+				intersectionPointsSphere2.putAll(rayIntersectionPoints2);
+
 			}
 			assertTrue(intersectionPointsSphere.get(sphere).size() == 2);
 			assertTrue(intersectionPointsSphere2.get(sphere2).size() == 9);
-			
+
 			System.out.println("Intersection Points:");
-			
-			//for (Point3D iPoint: intersectionPointsSphere)
-			intersectionPointsSphere.forEach((k,v) -> {
-				
-				for (Point3D iPoint: v) {
-					assertTrue(iPoint.equals(new Point3D(0.0, 0.0, -2.0)) ||
-						iPoint.equals(new Point3D(0.0, 0.0, -4.0)));
+
+			// for (Point3D iPoint: intersectionPointsSphere)
+			intersectionPointsSphere.forEach((k, v) -> {
+
+				for (Point3D iPoint : v) {
+					assertTrue(
+							iPoint.equals(new Point3D(0.0, 0.0, -2.0)) || iPoint.equals(new Point3D(0.0, 0.0, -4.0)));
 					System.out.println(iPoint);
 				}
 			});
 		}
 	}
-	
-	/*@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testSpherePoint3DDouble() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSphereSphere() {
-		fail("Not yet implemented");
-	}*/
+	/*
+	 * @Test public void testToString() { fail("Not yet implemented"); }
+	 * 
+	 * @Test public void testSpherePoint3DDouble() { fail("Not yet implemented"); }
+	 * 
+	 * @Test public void testSphereSphere() { fail("Not yet implemented"); }
+	 */
 
 }
